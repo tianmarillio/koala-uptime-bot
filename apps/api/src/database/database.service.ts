13be8ef5f7e5d65153a134ca640ac64bee9a3102
Tiansyah Marillio Pane <tiansyah.marillio@gmail.com>
2026-05-19 02:18:46 +0700

    Initial commit

---
+++ apps/api/src/database/database.service.ts
@@ -0,0 +1,16 @@
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { PrismaClient } from 'src/generated/prisma/client';
+
+@Injectable()
+export class DatabaseService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
